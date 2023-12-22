2ddfd0f249d7ad87b169396c7436a8e48140e6b7
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-22 20:00:25 +0530

    header section of game

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -4,23 +4,29 @@ export default function Game() {
   return (
     <div>
         <div className='flex gap-[593px] w-[1280px] h-[151px] mt-[64px] ml-[80px]'>
-            <div>
-                <div>
-                    <h1>0</h1>
+            <div className='w-[135px] h-[151px] mt-[-30px]'>
+                <div className='flex flex-col'>
+                    <p style={{fontSize:"100px",fontFamily:"poppins",fontWeight:"500"}}>0</p>
+                    <p className='mt-[-35px]' style={{fontSize:"24px",fontFamily:"poppins",fontWeight:"500"}} >Total Score</p>
                 </div>
-                <div>
-                    <h1>Total Score</h1>
+            </div>
+            <div className='w-[552px] h-[138px]  '>
+                <div className='flex gap-[30px] '>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>1</button>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>2</button>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>3</button>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>4</button>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>5</button>
+                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>6</button>
+                </div>
+                <div className='w-[182px] h-[36px] mt-4 ml-[431px]'>
+                    <h1 className='font-bold' style={{fontFamily:"poppins"}}>Select Number</h1>
                 </div>
                 
             </div>
-            <div>
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-            </div>
+            
+
+
         </div>
 
     </div>
