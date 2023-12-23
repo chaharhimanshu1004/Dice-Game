9b78cf011f2aff7df631bb8e7903ac05da9febb8
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-23 08:00:10 +0530

    Game Page UI

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { useState } from 'react';
+
+
 
 export default function Game() {
+
+    function generateRandom(){
+        let num=  Math.floor(Math.random() * 6) + 1;
+    }
+    
   return (
+
     <div>
         <div className='flex gap-[593px] w-[1280px] h-[151px] mt-[64px] ml-[80px]'>
             <div className='w-[135px] h-[151px] mt-[-30px]'>
@@ -10,7 +19,7 @@ export default function Game() {
                     <p className='mt-[-35px]' style={{fontSize:"24px",fontFamily:"poppins",fontWeight:"500"}} >Total Score</p>
                 </div>
             </div>
-            <div className='w-[552px] h-[138px]  '>
+            <div className='w-[552px] h-[138px] '>
                 <div className='flex gap-[30px] '>
                 <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>1</button>
                 <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>2</button>
@@ -24,8 +33,26 @@ export default function Game() {
                 </div>
                 
             </div>
-            
+        </div>
+        <div className='max-w-[250px] max-h-[449px] mt-[70px] ml-[595px] flex flex-col items-center justify-center'>
+            <div className='max-w-[170px] max-h-[160px]'>
+                <button>
+                    <img src='/images/assets/dice2.png' alt="cube image" />
+                </button>   
+            </div>
+            <div className='ml-1'>
+                <p className='mt-6' style={{fontFamily:"poppins",fontWeight:"600"}}>Click on dice to roll</p>
+            </div>
+            <div   className='flex mt-7 ml-1 flex-col gap-[18px]'>
+                <div className='rounded-sm'>
+                    <button style={{fontFamily:"poppins",fontWeight:"600",padding:"10px 15px 10px 15px"}} className='w-[157px] rounded-sm border-[1px] border-black'>Reset Score</button>
 
+                </div>
+                <div className='text-white rounded-sm bg-[rgba(0,0,0,1)]'>
+                    <button  style={{fontFamily:"poppins",fontWeight:"600",padding:"10px 15px 10px 15px"}} className='w-[157px] rounded-sm border-[1px] border-black'>Show rules</button>
+
+                </div>
+            </div>
 
         </div>
 
