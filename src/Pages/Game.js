a45d5da7ebfad71a6a08e2433d904c2cbc9298c1
himanshu1004200 <chaharhimanshu03@gmail.com>
2023-12-23 14:47:43 +0530

    button color toggle

--- src/Pages/Game.js
+++ src/Pages/Game.js
@@ -5,6 +5,17 @@ import { useState } from 'react';
 
 export default function Game() {
     const [currentNum,setCurrentNum] = useState(1);
+    const [userNum,setUserNum] = useState(undefined);
+    const [activeState,setActiveState] = useState(null);
+    const[score,setScore] = useState(0);
+    const[btnColor,setBtnColor] = useState([
+        {bg:'rgba(255,255,255,1)',text:'black'},
+        {bg:'rgba(255,255,255,1)',text:'black'},
+        {bg:'rgba(255,255,255,1)',text:'black'},
+        {bg:'rgba(255,255,255,1)',text:'black'},
+        {bg:'rgba(255,255,255,1)',text:'black'},
+        {bg:'rgba(255,255,255,1)',text:'black'},
+    ])
 
     function generateRandom(){
         return Math.floor(Math.random() * 6) + 1;
@@ -13,28 +24,51 @@ export default function Game() {
         const randomNum = generateRandom();
         setCurrentNum((prev)=>randomNum);
     }
-    console.log(currentNum);
     
+    function handleClick(e){
+        setUserNum(e.target.value);
+        setActiveState(e.target.value);
+        changeColor(e.target.value);
+    }
+    // onClick -- >> active state bna diya -->>
+    // then us active state pe sbke colors change kiya -->>  if active state h to color change krdo vrna white bg hi rehne do..hr bar re render har bar change krna h yeh
+    function changeColor(value){
+        const newColors = btnColor.map((btn, index) => {
+            return {
+              bg: index + 1 === parseInt(value, 10) ? 'black' : 'rgba(255,255,255,1)',
+              text: index + 1 === parseInt(value, 10) ? 'white' : 'black',
+            };
+        });
+        setBtnColor(newColors);
 
+    }
+    
+    console.log(btnColor)
+
+    // console.log(currentNum);
+    // console.log(userNum);
     
   return (
+
+    // 6 values --->> sirf ek value ko hi change to kaise??
 
     <div>
         <div className='flex gap-[593px] w-[1280px] h-[151px] mt-[64px] ml-[80px]'>
             <div className='w-[135px] h-[151px] mt-[-30px]'>
                 <div className='flex flex-col'>
-                    <p style={{fontSize:"100px",fontFamily:"poppins",fontWeight:"500"}}>0</p>
+                    <p style={{fontSize:"100px",fontFamily:"poppins",fontWeight:"500"}}>{score}</p>
                     <p className='mt-[-35px]' style={{fontSize:"24px",fontFamily:"poppins",fontWeight:"500"}} >Total Score</p>
                 </div>
             </div>
+            
             <div className='w-[552px] h-[138px] '>
                 <div className='flex gap-[30px] '>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>1</button>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>2</button>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>3</button>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>4</button>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>5</button>
-                <button className='border-[1px] border-black w-[72px] h-[72px] font-bold'>6</button>
+                <button value='1' onClick={handleClick}  style={{backgroundColor:btnColor[0].bg,color:btnColor[0].text}} className=' border-[1px] border-black w-[72px] h-[72px] font-bold'>1</button>
+                <button value='2' onClick={handleClick}  style={{backgroundColor:btnColor[1].bg,color:btnColor[1].text}}  className='border-[1px]  border-black w-[72px] h-[72px] font-bold'>2</button>
+                <button value='3' onClick={handleClick}  style={{backgroundColor:btnColor[2].bg,color:btnColor[2].text}} className='border-[1px]  border-black w-[72px] h-[72px] font-bold'>3</button>
+                <button value='4' onClick={handleClick}  style={{backgroundColor:btnColor[3].bg,color:btnColor[3].text}} className='border-[1px]  border-black w-[72px] h-[72px] font-bold'>4</button>
+                <button value='5' onClick={handleClick}  style={{backgroundColor:btnColor[4].bg,color:btnColor[4].text}} className='border-[1px]  border-black w-[72px] h-[72px] font-bold'>5</button>
+                <button value='6' onClick={handleClick}  style={{backgroundColor:btnColor[5].bg,color:btnColor[5].text}} className='border-[1px]  border-black w-[72px] h-[72px] font-bold'>6</button>
                 </div>
                 <div className='w-[182px] h-[36px] mt-4 ml-[431px]'>
                     <h1 className='font-bold' style={{fontFamily:"poppins"}}>Select Number</h1>
